0e2bdc39ba7a29707fe4ff396d37d988a938bc3c
Octav Zlatior <octav.zlatior@gmail.com>
2020-04-27 03:37:16 +0800

    Implements model state
    Remembering model state allows for multiple syncing of the db
    without reloading the same models again

--- src/dbcore.js
+++ src/dbcore.js
@@ -2,6 +2,9 @@
  * DB Core Class
  */
 
+const inflection = require("inflection");
+const pgsqlParser = require("pgsql-parser");
+
 const logger = require("./logger.js").getInstance().moduleBinding("DBCore", "db-one");
 
 const DBError = require("./dberror");
@@ -12,11 +15,19 @@ class DBCore {
 		logger.info("Creating DBCore Object");
 		this.sql = sql;
 		this.entities = {};
+		this.models = {};
+		this.associations = {};
 	}
 
 	createEntity (name, attributes, options) {
 		logger.info("Creating Entity " + name + ": " + Object.getOwnPropertyNames(attributes).join(", "),
 			"createEntity");
+		this.models[name] = {
+			name: name,
+			loaded: false,
+			attributes: attributes,
+			options: options
+		};
 		if (this.entities[name] !== undefined)
 			throw new DBError("Entity " + name + " already defined");
 		try {
@@ -38,6 +49,15 @@ class DBCore {
 		let sourceEntity = this.entities[source];
 		let targetEntity = this.entities[target];
 		let options = { through: through };
+		if (this.associations[source] === undefined)
+			this.associations[source] = {};
+		this.associations[source][target] = {
+			type: type,
+			source: source,
+			target: target,
+			through: through,
+			loaded: false
+		};
 		if (sourceEntity === undefined)
 			throw new DBError("Source entity " + src + " not found");
 		if (targetEntity === undefined)
@@ -64,7 +84,7 @@ class DBCore {
 		return true;
 	}
 
-	initialise (force) {
+	initialise (force, modelCb, associationCb) {
 		logger.info("Initialising Database force=" + (!!force), "initialise");
 		return new Promise((resolve, reject) => {
 			this.sql.authenticate()
@@ -72,12 +92,94 @@ class DBCore {
 					logger.info(
 						'Database connection to \'' + this.sql.config.host + '/' + this.sql.config.database + '\' successful',
 						"initialise");
-					this.sql.sync({force: force}).then(() => {
-						logger.info('Sync succesful', "loadModels");
+					this.sync(force, modelCb, associationCb).then(() => {
+						logger.info('Sync succesful', "initialise");
 						resolve();
 					}).catch(reject);
 				})
 				.catch(reject);
+		});
+	}
+
+	syncLogFn (modelCb, associationCb, msg) {
+		logger.sql(msg, "sync");
+		if (!modelCb && !associationCb)
+			return;
+		let query = msg.replace(/Executing \([^\(\)]*\): /, "");
+		query = pgsqlParser.parse(query).query;
+		let stmt = query[0].RawStmt.stmt.CreateStmt;
+		if (!stmt)
+			return;
+		let relName = stmt.relation.RangeVar.relname;
+		let modelName = inflection.singularize(relName);
+		let cols = [];
+		let assocs = [];
+		stmt.tableElts.map((el) => {
+			if (el && el.ColumnDef) {
+				let colname = el.ColumnDef.colname;
+				cols.push(colname);
+				if (el.ColumnDef.constraints === undefined)
+					return;
+				for (let i=0; i<el.ColumnDef.constraints.length; i++) {
+					if (el.ColumnDef.constraints[i].Constraint.contype === 8) { // foreign key
+						let target = el.ColumnDef.constraints[i].Constraint.pktable.RangeVar.relname;
+						target = inflection.singularize(target);
+						if (this.models[target])
+							assocs.push(target);
+						else
+							logger.warn("No model found for association target " + target, "sync.syncLogFn");
+					}
+				}
+			}
+		});
+		// is it a model?
+		if (this.models[modelName]) {
+			this.models[modelName].loaded = true;
+			modelCb(modelName);
+			if (!associationCb)
+				return;
+			// check for associations
+			if (this.associations[modelName]) {
+				for (let i=0; i<assocs.length; i++) {
+					if (this.associations[modelName][assocs[i]]) {
+						this.associations[modelName][assocs[i]].loaded = true;
+						associationCb(modelName, assocs[i]);
+					}
+					else
+						logger.warn("No association between " + modelName + " and " + target, "sync.syncLogFn");
+				}
+			}
+			else if (assocs.length)
+				logger.warn("No association found for model " + modelName + " (" + assocs.join(", ") + ")", "sync.syncLogFn");
+			return;
+		}
+		if (!associationCb)
+			return;
+		// is it a many-to-many association table?
+		for (let i=0; i<assocs.length; i++) {
+			for (let j=0; j<assocs.length; j++) {
+				if (i === j)
+					continue;
+				if (this.associations[assocs[i]] && this.associations[assocs[i]][assocs[j]]) {
+					this.associations[assocs[i]][assocs[j]].loaded = true;
+					associationCb(assocs[i], assocs[j]);
+				}
+				else
+					logger.warn("No association between " + assocs[i] + " and " + assocs[j], "sync.syncLogFn");
+			}
+		}
+	}
+
+	sync (force, modelCb, associationCb) {
+		logger.info("Running sync", "sync");
+		return new Promise((resolve, reject) => {
+			this.sql.sync({
+				force: force,
+				logging: this.syncLogFn.bind(this, modelCb, associationCb)
+			}).then(() => {
+				logger.info('Sync succesful', "sync");
+				resolve();
+			}).catch(reject);
 		});
 	}
 
--- src/modelloader.js
+++ src/modelloader.js
@@ -4,28 +4,41 @@
 
 const inflection = require("inflection");
 
+const object = require("util-one").object;
+
 const logger = require("./logger.js").getInstance().moduleBinding("ModelLoader", "db-one");
 
+const DBCore = require("./dbcore");
 const DBError = require("./dberror");
 
 class ModelLoader {
 
 	constructor (dbCore) {
 		logger.info("Creating ModelLoader Object");
-		this.dbCore = dbCore;
-		this.models = {};
-		this.associations = {};
-		this.ownershipModel = {
-			owner: null,
-			group: null
-		};
+		if (dbCore instanceof DBCore) {
+			this.dbCore = dbCore;
+			this.models = {};
+			this.associations = {};
+			this.ownershipModel = {
+				owner: null,
+				group: null
+			};
+		}
+		// try a copy-like constructor
+		else if (dbCore.dbCore instanceof DBCore) {
+			this.dbCore = dbCore.dbCore;
+			this.models = object.deepCopy(dbCore.models);
+			this.associations = object.deepCopy(dbCore.associations);
+			this.ownershipModel = object.deepCopy(dbCore.ownershipModel);
+		}
 	}
 
 	loadModels (models) {
 		logger.info("Load Models, count = " + models.length, "loadModels");
 		// create models
 		for (let i in models) {
-			this.models[models[i].name] = models[i];
+			this.models[models[i].name] = object.deepCopy(models[i]);
+			this.models[models[i].name].loaded = false;
 			this.associations[models[i].name] = {};
 		}
 		// create associations
@@ -34,6 +47,7 @@ class ModelLoader {
 				continue;
 			for (let j=0; j<models[i].associations.length; j++) {
 				let toPush = {
+					loaded: false,
 					type: models[i].associations[j].type,
 					source: models[i].name,
 					target: models[i].associations[j].target
@@ -100,19 +114,44 @@ class ModelLoader {
 		return ret;
 	}
 
+	isModelInitialised (model) {
+		if (this.models[model] === undefined)
+			throw new DBError("No such model " + model);
+		return (this.models[model].loaded === true);
+	}
+
+	isAssociationInitialised (source, target) {
+		if (this.associations[source] === undefined || this.association[source][target] === undefined)
+			throw new DBError("No association found between " + source + " and " + target);
+		return (this.associations[source][target].loaded === true);
+	}
+
 	dbInit (force) {
 		logger.info("Setting up Database force=" + force, "dbInit");
-		for (let i in this.models)
+		for (let i in this.models) {
+			if (this.models[i].loaded)
+				continue;
 			this.dbCore.createEntity(i, this.models[i].attributes, this.models[i].options);
+		}
 		for (let i in this.associations) {
 			for (let j in this.associations[i]) {
+				if (this.associations[i][j].loaded)
+					continue;
 				this.dbCore.createAssociation(this.associations[i][j].type, this.associations[i][j].source,
 					this.associations[i][j].target, this.associations[i][j].through);
 			}
 		}
 		return new Promise((resolve, reject) => {
-			this.dbCore.initialise(force).then(resolve).catch(reject);
+			this.dbCore.initialise(force,
+				(model) => { this.models[model].loaded = true; },
+				(source, target) => { this.associations[source][target].loaded = true; }
+			).then(resolve).catch(reject);
 		});
+	}
+
+	dbSync () {
+		logger.info("Syncing Database", "dbSync");
+		return this.dbInit(false);
 	}
 
 }
